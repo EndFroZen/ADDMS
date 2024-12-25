371d5604d7f2a6d4884899d3e6b37c7529eb1f53
EndFroZen <Ef.chanachol@gmail.com>
2024-12-25 16:04:56 +0700

    update v1

--- server/addon/crateDomain.go
+++ server/addon/crateDomain.go
@@ -24,24 +24,7 @@ func CreateDomain(c *fiber.Ctx) error {
 
 	cmd = exec.Command("cmd", "/C", "cd ../create/user1 && mkdir ", domain, "&& echo suc")
 
-	ouput, err = cmd.CombinedOutput()
-	s = string(ouput)
-	fmt.Println(s)
-	if err != nil {
-		return c.SendStatus(fiber.ErrBadRequest.Code)
-	}
-	if s == "suc" {
-		fmt.Println("awdwdgatywdf")
-		fmt.Println(langue)
-		// if langue == "nodejs" {
-		// 	cmd = exec.Command("cmd", "/C", "cd ../create/user1/", domain, " && dir")
-		// 	ouput, err = cmd.CombinedOutput()
-		// 	fmt.Println(string(ouput))
-		// 	if err != nil {
-		// 		return c.SendString(err.Error())
-		// 	}
-		// }
-	}
+	
 
 	return c.Redirect("http://localhost:3000/dashboard")
 }
