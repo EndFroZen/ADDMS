b17c70575d225ab828aaaded399aaa51e3594e77
EndFroZen <Ef.chanachol@gmail.com>
2025-04-29 10:06:23 +0700

    update 0.1.1

--- server/server.go
+++ server/server.go
@@ -26,29 +26,27 @@ func main() {
 		return c.SendString(showdata)
 	})
 	app.Post("/register",func (c *fiber.Ctx)error  {
-		email := c.FormValue("email")
-		username := c.FormValue("username")
-		password := c.FormValue("password")
-		registserUser := models.User{
-			Email: email,
-			Username: username,
-			Password: password,
+		registerUser := new(models.User)
+		if err := c.BodyParser(registerUser);err != nil{
+			return c.Status(fiber.ErrBadRequest.Code).JSON(function.SimpleStatus(400,"Missing body client"))
 		}
-		if err := usermanage.Register(config.DB ,&registserUser) ;err != nil{
-			return c.SendString(fiber.ErrBadRequest.Message)
+		if err := usermanage.Register(config.DB ,registerUser) ;err != nil{
+			return c.Status(fiber.ErrBadRequest.Code).JSON(function.SimpleStatus(400,"Registration failed or User already exists"))
 		}
-		return c.SendString("register succesfull")
+		return c.Status(fiber.StatusOK).JSON(function.SimpleStatus(200,"Register successful"))
 	})
 	app.Post("/login",func (c *fiber.Ctx)error  {
 		loginUserNew  := new(models.User)
 		if err := c.BodyParser(loginUserNew);err != nil {
-			return c.SendString(fiber.ErrBadRequest.Message)
+			return c.Status(fiber.StatusBadRequest).JSON(function.Statustoken(400, "Missing body client","null"))
+			
 		}
 		token ,err := usermanage.Login(config.DB,loginUserNew)
 		if err!=nil{
-			return c.SendString(fiber.ErrBadRequest.Message)
+			return c.Status(fiber.StatusBadRequest).JSON(function.Statustoken(404, "Not found user!","nul"))
 		}
-		return c.SendString(token)
+		return c.Status(fiber.StatusOK).JSON(function.Statustoken(200, "Login successful",token))
 	})
 	app.Listen("127.0.0.1:3001")
 }
+
